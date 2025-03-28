c2dd3ad9be0bed46382cf6a7baf819ffc5294b36
Sushobhan Tripathy <trisusthegrt03@gmail.com>
2025-03-28 11:11:26 +0530

    first commit

---
+++ A_Watermelon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i;
+    cin>>i;
+    if(i>2 && i%2==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+ return 0;
+}
