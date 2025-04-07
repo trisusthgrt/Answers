4d22360b4e8f4c952148b174f779dbf9f334d0df
Sushobhan Tripathy <trisusthegrt03@gmail.com>
2025-04-07 16:19:04 +0530

    7

---
+++ A_Sereja_and_Dima.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define NO cout << "NO\n"
+#define YES cout << "YES\n"
+#define pb push_back
+#define ff first 
+#define ss second 
+#define input(A) for (auto &i : A) cin >> i
+#define output(A) for (auto &i : A) cout << i << " "
+const int MAXN = 1e6;
+const int MOD = 1e9 + 7;
+ll fac[MAXN + 1];
+ll inv[MAXN + 1];
+ll exp(ll x, ll n, ll m) {
+    x %= m;
+    ll res = 1;
+    while (n > 0) {
+        if (n % 2 == 1) {
+            res = res * x % m;
+        }
+        x = x * x % m;
+        n /= 2;
+    }
+    return res;
+}
+void factorial() {
+    fac[0] = 1;
+    for (int i = 1; i <= MAXN; i++) {
+        fac[i] = fac[i - 1] * i % MOD;
+    }
+}
+void inverses() {
+    inv[MAXN] = exp(fac[MAXN], MOD - 2, MOD);
+    for (int i = MAXN; i >= 1; i--) {
+        inv[i - 1] = inv[i] * i % MOD;
+    }
+}
+ll choose(int n, int r) {
+    return fac[n] * inv[r] % MOD * inv[n - r] % MOD;
+}
+int main() {
+  int n;
+  cin >> n;
+  vector<int> cards(n);
+  for (int i = 0; i < n; i++) {
+      cin >> cards[i];
+  }
+
+  int left = 0, right = n - 1;
+  int sereja = 0, dima = 0;
+  bool turn = true; // true for Sereja, false for Dima
+
+  while (left <= right) {
+      int pick;
+      if (cards[left] > cards[right]) {
+          pick = cards[left];
+          left++;
+      } else {
+          pick = cards[right];
+          right--;
+      }
+
+      if (turn)
+          sereja += pick;
+      else
+          dima += pick;
+
+      turn = !turn; // alternate turns
+  }
+
+  cout << sereja << " " << dima << endl;
+
+  return 0;
+}
